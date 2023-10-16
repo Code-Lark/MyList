623b8283d168e52d5fc654e293c57949be4b1deb
Code-Lark <code2lark@gmail.com>
2023-10-16 17:00:25 +0800

    2023-10-16

--- index.php
+++ index.php
@@ -13,32 +13,28 @@ require_once("./config.php");
     echo "<table style='border: solid 1px black;'>";
     echo "<tr><th>Id</th><th>Firstname</th><th>Lastname</th></tr>";
      
-    class TableRows extends RecursiveIteratorIterator {
-        function __construct($it) { 
-            parent::__construct($it, self::LEAVES_ONLY); 
-        }
-     
-        function current() {
-            return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
-        }
     
-    } 
     $sql ='select * from incident';
     try {
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $stmt = $conn->prepare($sql); 
-        $stmt->execute();
-     
-        // 设置结果集为关联数组
-        $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
-        foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) { 
-            echo $v;
+
+        if ($stmt->execute()) {
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                $products[] = $row;
+                // print_r($products);
+
+                echo $products["id"];
+                echo $products['incident'];
+            
+            }
         }
     }
     catch(PDOException $e) {
         echo "Error: " . $e->getMessage();
     }
+    $conn=null;
     ?>
 </body>
 </html>
